0995f382bb0c260ff73d2de822ac1d7a310bdbaa
Taures <taurest1988@gmail.com>
2023-09-16 12:38:26 +0300

    Задание 25.2 скорректировал файл paginators.py

--- studies/paginators.py
+++ studies/paginators.py
@@ -3,3 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class StudiesPaginator(PageNumberPagination):
     page_size = 6
+    page_size_query_param = 'page_size'
+    max_page_size = 30
