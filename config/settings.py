ec3b060ca31414bcb255e710aafe4028d4debf2b
Taures <taurest1988@gmail.com>
2023-09-24 15:28:11 +0300

    Задание 25.2 обновил, изменил тесты

--- config/settings.py
+++ config/settings.py
@@ -36,11 +36,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework.authtoken',
     'rest_framework',
     'rest_framework_simplejwt',
     'users',
     'studies',
     'django_filters',
+
 ]
 
 MIDDLEWARE = [
@@ -136,7 +138,11 @@ LOGIN_REDIRECT_URL = '/'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    )
 }
--- studies/tests.py
+++ studies/tests.py
@@ -1,19 +1,20 @@
-from django.urls import reverse
+from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from studies.models import Course, Lesson
 from users.models import User, UserRoles
 
 
-class LessonTestCase(APITestCase):
+class LessonCreateTest(APITestCase):
 
     def setUp(self):
         """Заполнение первичных данных"""
 
-        self.user = User.objects.create(
+        self.user = get_user_model().objects.create(
             email='taures_88@mail.ru',
+            first_name='Anton',
+            last_name='Turchenko',
             is_staff=True,
             is_superuser=True,
             is_active=True,
@@ -22,123 +23,157 @@ class LessonTestCase(APITestCase):
         self.user.set_password('Fynjybj88')
         self.user.save()
 
-        token = RefreshToken.for_user(self.user)
-        self.access_token = str(token.access_token)
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+        self.course = Course.objects.create(
+            title='Existing Course',
+            desc='Existing Description'
+        )
+
+    def test_lesson_create(self):
+        """Тест для создания уроков"""
+
+        self.client.force_authenticate(user=self.user)
+        data = {
+            'title': 'Test1Lesson',
+            'desc': 'Test1Lesson',
+            'link': 'https://www.youtube.com/watch?v=abc123',
+            'course_lesson': self.course.id,
+            'buyer': self.user.id
+        }
+        response = self.client.post(
+            '/lesson/create/',
+            data=data
+        )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+class LessonDeleteTest(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create(
+            email='taures_88@mail.ru',
+            first_name='Anton',
+            last_name='Turchenko',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+            role=UserRoles.moderator,
+        )
+        self.user.set_password('Fynjybj88')
+        self.user.save()
 
         self.course = Course.objects.create(
-            title='Фитнес',
+            title='Existing Course',
+            desc='Existing Description'
         )
 
         self.lesson = Lesson.objects.create(
-            title='Бодибилдинг',
-            course_lesson=self.course
+            title='Test1Lesson',
+            desc='Test1Lesson',
+            link='https://www.youtube.com/watch?v=abc123',
+            course_lesson=self.course,
         )
 
-    def test_get_list(self):
-        """Тест получения списка уроков"""
-
-        response = self.client.get(
-            reverse('studies:lesson-list')
+    def test_delete_lesson(self):
+        """ Тестирование удаления урока """
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(
+            f'/lesson/delete/{self.lesson.id}/',
         )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_204_NO_CONTENT
+        )
+
+        self.assertFalse(Lesson.objects.filter(id=self.lesson.id).exists())
+
+
+class LessonListViewTest(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create(
+            email='taures_88@mail.ru',
+            first_name='Anton',
+            last_name='Turchenko',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+            role=UserRoles.moderator,
+        )
+        self.user.set_password('Fynjybj88')
+        self.user.save()
+
+        self.course = Course.objects.create(
+            title='Existing Course',
+            desc='Existing Description'
+        )
+
+        self.lesson = Lesson.objects.create(
+            title='Test1Lesson',
+            desc='Test1Lesson',
+            link='https://www.youtube.com/watch?v=abc123',
+            course_lesson=self.course,
+        )
+
+    def test_list_lessons(self):
+        """ Тестирование на отображение урока """
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.get('/lesson/')
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK)
+
+        self.assertEqual(len(response.data), 4)
+
+
+class LessonUpdateTest(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create(
+            email='taures_88@mail.ru',
+            first_name='Anton',
+            last_name='Turchenko',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+            role=UserRoles.moderator,
+        )
+        self.user.set_password('Fynjybj88')
+        self.user.save()
+
+        self.course = Course.objects.create(
+            title='Existing Course',
+            desc='Existing Description'
+        )
+
+        self.lesson_data = {
+            'title': 'Test1Lesson',
+            'desc': 'Test1Lesson',
+            'preview': 'https://example.com/preview.jpg',
+            'link': 'https://www.youtube.com/watch?v=abc123',
+            'course_lesson': self.course,
+        }
+        self.lesson = Lesson.objects.create(**self.lesson_data)
+
+    def test_update_lesson(self):
+        """ Тестирование обновления урока """
+        self.client.force_authenticate(user=self.user)
+        data = {
+            'title': 'Update Lesson',
+            'desc': 'Update lesson',
+            'link': 'https://www.youtube.com/watch?v=update45',
+            'course_lesson': self.course.id,
+        }
+        response = self.client.put(
+            f'/lesson/update/{self.lesson.id}/',
+            data=data,
+        )
+
         self.assertEqual(
             response.status_code,
             status.HTTP_200_OK
         )
-
-        self.assertEqual(
-            response.json(),
-            {
-                "count": 1,
-                "next": None,
-                "previous": None,
-                "results": [
-                    {
-                        "pk": self.lesson.id,
-                        "title": self.lesson.title,
-                        "preview": self.lesson.preview,
-                        "link": self.lesson.link,
-                        "course_lesson": self.lesson.course_lesson.title,
-                        "buyer": self.lesson.buyer
-                    }
-                ]
-            }
-        )
-
-    def test_lesson_create(self):
-        """Тест создания уроков"""
-
-        response = self.client.post('/lesson/create/',
-                                    {'title': 'test lesson', 'desc': 'test lesson 1',
-                                     'link': 'https://www.youtube.com/testlesson1', 'buyer': 1})
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_lesson_update(self):
-        """ Тест обновления урока """
-        self.client.force_authenticate(user=self.user)
-
-        response = self.client.patch(reverse('studies:lesson-update', args=[self.lesson.pk]),
-                                     {'title': 'test lesson', 'desc': 'test lesson 1',
-                                      'link': 'https://www.youtube.com/testlesson1', 'buyer': 1})
-
-        self.assertEqual(
-            response.status_code, status.HTTP_200_OK,
-        )
-
-        self.assertTrue(
-            Lesson.objects.all().exists()
-        )
-
-    def test_lesson_delete(self):
-
-        """ Тест удаления урока """
-
-        self.client.force_authenticate(user=self.user)
-
-        response = self.client.delete(reverse('studies:lesson-delete', args=[self.lesson.pk]))
-
-        self.assertEqual(
-            response.status_code, status.HTTP_204_NO_CONTENT,
-        )
-        self.assertFalse(
-            Lesson.objects.all().exists(),
-        )
-
-    def tearDown(self) -> None:
-        self.user.delete()
-        self.course.delete()
-        self.lesson.delete()
-
-
-class SubscriptionTestCase(APITestCase):
-    def setUp(self):
-        """Заполнение первичных данных"""
-
-        self.user = User.objects.create(
-            email='test1@mail.com',
-            is_staff=False,
-            is_superuser=False,
-            is_active=True,
-            role=UserRoles.the_user,
-        )
-        self.user.set_password('qwerty12345')
-        self.user.save()
-
-        token = RefreshToken.for_user(self.user)
-        self.access_token = str(token.access_token)
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
-
-        self.course = Course.objects.create(
-            title='Музыка'
-        )
-
-        self.lesson = Lesson.objects.create(
-            title='Игра на гитаре',
-            course_lesson=self.course
-        )
-
-    def test_create_subscription(self):
-        response = self.client.post('/subscription/create/',
-                                    {'course': self.course.id, 'user': self.user.id, 'status_subscription': False})
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.lesson.refresh_from_db()
+        self.assertEqual(self.lesson.title, data['title'])
+        self.assertEqual(self.lesson.desc, data['desc'])
+        self.assertEqual(self.lesson.link, data['link'])
