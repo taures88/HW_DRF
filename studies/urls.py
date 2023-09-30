40f981542166ad08cf1e879a2b7e02c1dca2b5c1
Taures <taurest1988@gmail.com>
2023-09-30 11:41:50 +0300

    Задание 26.1 выполнил

--- studies/urls.py
+++ studies/urls.py
@@ -4,7 +4,8 @@ from studies.apps import StudiesConfig
 from rest_framework.routers import DefaultRouter
 
 from studies.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView, PaymentListAPIView, SubscriptionCreateAPIView, SubscriptionDestroyAPIView
+    LessonUpdateAPIView, LessonDestroyAPIView, PaymentListAPIView, SubscriptionCreateAPIView, \
+    SubscriptionDestroyAPIView, PaymentDeleteAPIView, PaymentCreateAPIView, PaymentUpdateAPIView, PaymentDetailAPIView
 
 app_name = StudiesConfig.name
 
@@ -18,7 +19,11 @@ urlpatterns = [
                   path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'), # редактор уроков
                   path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'), # удаление уроков
 
-                  path('payment/', PaymentListAPIView.as_view(), name='payment_list'), # список оплат
+                  path('payment/', PaymentListAPIView.as_view(), name='payment-list'), # список оплат
+                  path('payment/<int:pk>/', PaymentDetailAPIView.as_view(), name='payment-detail'),# информация об определенной оплате
+                  path('payment/update/<int:pk>/', PaymentUpdateAPIView.as_view(), name='payment-update'),# обновление
+                  path('payment/create/', PaymentCreateAPIView.as_view(), name='payment-create'),# создание платежа
+                  path('payment/delete/<int:pk>/', PaymentDeleteAPIView.as_view(), name='payment-delete'),# удаление платежа
 
                   path('subscription/create/', SubscriptionCreateAPIView.as_view(), name='subscription-create'),
                   path('subscription/delete/<int:pk>/', SubscriptionDestroyAPIView.as_view(),
